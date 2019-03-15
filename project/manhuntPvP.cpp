0f5d31ae89be4bf4ae8c453a3b093a7458f74af5
Majed Almazrouei <malmazr2@ix-dev.cs.uoregon.edu>
2019-03-15 11:29:55 -0700

    Added the print function

--- project/manhuntPvP.cpp
+++ project/manhuntPvP.cpp
@@ -61,3 +61,135 @@ BoardType::BoardType(){
   enemyY = y;
   array[enemyX][enemyY] = 'E';
 }
+
+BoardType::BoardType(int x){
+	
+	loadBoard();
+	//array[playerX][playerY] = 'P';
+	cout << "LOADED: " <<playerX<<" "<<playerY<<" "<<botX<<" "<<botY<<" "<<size<<endl;
+  int i, j;
+  string **boardArr = nullptr;
+	//Initializes dynamic array for the board
+  boardArr = new string * [size];
+  for (i = 0; i < size; i++) {
+    boardArr[i] = new string [size];
+    // Set values
+    for (j = 0; j < size; j++){
+      boardArr[i][j] = "-";
+    }
+  }
+  
+  
+  // Initialize to player 1, and start of game
+  emptySlot = "-";
+  size = size;
+  array = boardArr;
+  isFinished = false;
+  //printBoard();
+  userInput = 0;
+  array[playerX][playerY] = 'P';
+	array[enemyX][enemyY] = "E";
+	
+}
+
+void BoardType::printBoard(){
+	
+	printThreat();
+	cout << "      ";
+	for (int i = 0; i < size; i++)
+	{
+       cout << i << "  ";
+    }
+
+	cout << "\n";
+
+	for (int i = 0; i < size; i++)
+    {
+		cout << i << "     ";
+		for (int j = 0; j < size; j++)
+	{
+		cout << array[i][j] << "  ";
+	}
+		cout << "\n";
+    }
+  cout << "\n";
+
+}
+
+// This function prints the threat bar.
+void BoardType::printThreat(){
+	int threatCount;
+	
+	int diffX = (playerX - enemyX);
+	// This is to ensure absolute value.
+	if (diffX < 0){
+		diffX = diffX * (-1);
+	}
+	
+	int diffY = (playerY - enemyY);
+	// This is to ensure absolute value.
+	if (diffY < 0){
+		diffY = diffY * (-1);
+	}
+	
+	// We check for the greater number, to ensure the
+	// right possible area of the opponent.
+	if (diffX > diffY){
+		// as difference decreases, threat increases.
+		// At the same time, the possible area becomes
+		// smaller (in terms of strategy).
+		if(diffX == 5){
+			threatCount = 1;
+		}
+		else if(diffX == 4){
+			threatCount = 2;
+		}
+		else if(diffX == 3){
+			threatCount = 3;
+		}
+		else if(diffX == 2){
+			threatCount = 4;
+		}
+		else if(diffX == 1){
+			threatCount = 5;
+		}
+		else{
+			threatCount = 0;
+		}
+	}
+	
+	// We check for the greater number, to ensure the
+	// right possible area of the opponent.	
+	else if (diffX <= diffY){
+		// as difference decreases, threat increases.
+		// At the same time, the possible area becomes
+		// smaller (in terms of strategy).
+		if(diffY == 5){
+			threatCount = 1;
+		}
+		else if(diffY == 4){
+			threatCount = 2;
+		}
+		else if(diffY == 3){
+			threatCount = 3;
+		}
+		else if(diffY == 2){
+			threatCount = 4;
+		}
+		else if(diffY == 1){
+			threatCount = 5;
+		}
+		else{
+			threatCount = 0;
+		}
+	}	
+	
+	//Printing the bars of threat detector
+	//depending on threat count variable calculated above.
+	cout << "Threat Count--> ";
+	for (int i = 0; i < threatCount; i++){
+		cout << "[]";
+	}
+	cout << endl;
+	
+}
