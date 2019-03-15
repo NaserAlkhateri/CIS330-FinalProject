5ee41a2826418014a37a2c03a9236c1ed383e6d3
Majed Almazrouei <malmazr2@ix-dev.cs.uoregon.edu>
2019-03-15 11:40:10 -0700

    Finished main file for PvP mode

--- project/testManhuntPvP.cpp
+++ project/testManhuntPvP.cpp
@@ -1,2 +1,28 @@
 //Author: Ahmed Al Ali, Naser Al Khateri, Majed Al Mazrouei
 //Main file for PvP mode.
+
+#include <iostream>
+#include <string>
+#include "manhunt.hpp"
+using namespace std;
+
+int main ()
+{
+	int beginChoice = 0; 
+	cout << "Welcome to Manhunt!" << endl;
+	cout << "1.New Game\n2.Load Game" << endl;
+	cin >> beginChoice;
+	if (beginChoice == 1){
+  BoardType board;
+  
+  board.printBoard();  
+	board.changeBoard();
+	}if (beginChoice == 2){
+		BoardType b(1);
+		b.printBoard();
+		b.changeBoard();
+	}
+	
+  return 0;
+ 
+}
