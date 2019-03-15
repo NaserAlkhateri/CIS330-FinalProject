f50ff3f9c6f0158aaab126234cdc64402f928c36
Majed Almazrouei <malmazr2@ix-dev.cs.uoregon.edu>
2019-03-15 13:43:40 -0700

    more options to run shell script

--- project/run.sh
+++ project/run.sh
@@ -1,6 +1,8 @@
 read -p "- To play vs computer enter (1)
 - To play vs player   enter (2)
 - To make clean       enter (3)
+- To check memory leak for PVC enter (4)
+- To check memory leak for PVP enter (5)
 " make
 case "$make" in
     1) make manhunt.exe
@@ -8,5 +10,7 @@ case "$make" in
 	2) make manhuntPvP.exe
 	   ./manhuntPvP.exe;;
     3) make clean ;;
+	4) make memcheckManhunt.exe ;;
+	5) make memcheckManhuntPvP.exe ;;
     *) echo "Wrong choice " ;;
 esac
