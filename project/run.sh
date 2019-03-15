00cb13652b78237039e35a530da116dc6e9b3748
Majed Almazrouei <malmazr2@ix-dev.cs.uoregon.edu>
2019-03-15 00:01:28 -0700

    Added a bash run shell

---
+++ project/run.sh
@@ -0,0 +1,9 @@
+read -p "- To play vs computer enter (1)
+- To play vs player   enter (2)
+- To make clean       enter (3)
+" make
+case "$make" in
+    1) make manhunt.exe ;;
+    3) make clean ;;
+    *) echo "Wrong choice " ;;
+esac
