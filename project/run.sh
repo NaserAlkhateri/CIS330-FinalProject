8beeecb06c4efa139b1a662f57712ce022c8b729
Majed Almazrouei <malmazr2@ix-dev.cs.uoregon.edu>
2019-03-15 00:07:32 -0700

    Added more option to the shell script

--- project/run.sh
+++ project/run.sh
@@ -3,7 +3,8 @@ read -p "- To play vs computer enter (1)
 - To make clean       enter (3)
 " make
 case "$make" in
-    1) make manhunt.exe ;;
+    1) make manhunt.exe
+	   ./manhunt.exe;;
     3) make clean ;;
     *) echo "Wrong choice " ;;
 esac
